98913da1d9abea7f905873d6a9000d625e8f6312
BigChungusssss <2550283@students.wits.ac.za>
2026-09-06 21:35:33 +0200

    fixed server, added PATCH

--- backend_backend/server.js
+++ backend_backend/server.js
@@ -26,8 +26,9 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
   
   if (req.method === 'OPTIONS') {
     return res.sendStatus(200);
